9771cc188072232315ab373af03655c6a050b03a
Wenbo Xie <weboxie@umass.edu>
2017-08-10 14:41:33 -0400

    Aug_10_2017_14:41

--- run_vector.sh
+++ run_vector.sh
@@ -9,11 +9,29 @@ if [ -f running_log_vector.txt ];
 fi 
 
 n=1
+rate_func=1
+number_of_trails=10
 
-while [ $n -le 2 ]
+
+while [ $n -le $number_of_trails ]
 do
 	echo "Iteration: $n"
 	echo "Iteration: $n" >> running_log_vector.txt
-	./1D_new_vector_parallel $n >> running_log_vector.txt
+	./1D_new_vector_parallel $rate_func >> running_log_vector.txt
 	n=$(( n + 1 ))
 done
+
+n=1
+rate_func=2
+
+while [ $n -le $number_of_trails ]
+do
+	echo "Iteration: $n"
+	echo "Iteration: $n" >> running_log_vector.txt
+	./1D_new_vector_parallel $rate_func >> running_log_vector.txt
+	n=$(( n + 1 ))
+done
+
+unset n
+unset number_of_trails
+unset rate_func
